9ec82e27d8c375c1e257e34fb86bed70035de267
Marlos C. Machado <machado@cs.ualberta.ca>
2016-10-08 19:51:42 -0600

    Skeleton for learning algorithm.

---
+++ Learning.py
@@ -0,0 +1,15 @@
+'''
+This class implements value iteration so we can solve an MDP and extract the
+optimal policy. These learned policies will compose an option to be specified.
+
+Author: Marlos C. Machado
+'''
+import numpy as np
+
+class Learning:
+
+	def __init__(self):
+		something = 0
+
+	def solveValueIteration(self):
+		solveMDP = 0
