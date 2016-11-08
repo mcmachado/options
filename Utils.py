36d0ae916313c2031ab317922f636924a25053d6
Marlos C. Machado <marlos.cholodovskis@gmail.com>
2016-11-07 20:04:44 -0700

    One can load an option that was designed by a human.

--- Utils.py
+++ Utils.py
@@ -22,3 +22,47 @@ class Utils:
 					expM[i][j] = M[i][j]**exp
 
 		return expM
+
+	@staticmethod
+	def _readFile(path):
+		''' We just read the file and put its contents in fstr.'''
+		fstr = ''
+		file = open(path, 'r')
+		for line in file:
+			fstr += line
+
+		return fstr
+
+	@staticmethod
+	def loadOption(filePath):
+		''' I now parse the received string. I'll store everything in a matrix
+		(matrixMDP) such that U, D, R, L, T mean 'up', 'down', 'right', 'left',
+		'terminate', referring to the proper action to be taken.'''
+		fstr = Utils._readFile(filePath)
+		data = fstr.split('\n')
+		numRows = int(data[0].split(',')[0])
+		numCols = int(data[0].split(',')[1])
+		matrixMDP = np.zeros((numRows, numCols), dtype = np.int)
+
+		for i in xrange(len(data) - 1):
+			for j in xrange(len(data[i+1])):
+				if data[i+1][j] == 'X':
+					matrixMDP[i][j] = 4 #terminate
+				elif data[i+1][j] == 'T':
+					matrixMDP[i][j] = 4 #terminate
+				elif data[i+1][j] == 'L':
+					matrixMDP[i][j] = 3 #left
+				elif data[i+1][j] == 'D':
+					matrixMDP[i][j] = 2 #down
+				elif data[i+1][j] == 'R':
+					matrixMDP[i][j] = 1 #right
+				elif data[i+1][j] == 'U':
+					matrixMDP[i][j] = 0 #up
+
+
+		option = []
+		for i in xrange(numRows):
+			for j in xrange(numCols):
+				option.append(matrixMDP[i][j])
+
+		return option
